dafb33b6b540d76800bfbdcdd02e112ff45659bf
axinawang <axinawang@163.com>
2018-02-08 10:33:42 +0800

    客户添加完成

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 //app.js
-var SERVER_URL = "http://127.0.0.1:8080/jk_web/";
+
 App({
   // 全局变量
   globalData:{
+    header: { 'Cookie': '' }, //这里还可以加入其它需要的请求头，比如'x-requested-with': 'XMLHttpRequest'表示ajax提交，微信的请求时不会带上这个的
+    serverUrl :'http://127.0.0.1:8080/jk_web/',
     userInfo:null,//用户信息
     location:null,//经纬度信息
     city:{"city":"郑州","province":"河南","district":""},//用户城市信息
     bid:null,//用户bid
     systemInfo : null,//系统信息
     shopCarGoods:{},//购物车商品
-    islogin : false,//是否登录
-    userInfo:null,//用户信息
+    islogin : false//是否登录
+
   },
-  onLaunch: function () {
+  onLaunch: function () {    
+    var that = this;
     wx.login({
       success: function (res) {
         var code = res.code;
@@ -21,8 +24,12 @@ App({
 
           // --------- 发送凭证 ------------------
           wx.request({
-            url: SERVER_URL+'loginFromWeixin.action',
-            data: { weixincode: code }
+            url: that.globalData.serverUrl+'loginFromWeixin.action',
+            data: { weixincode: code },
+            success:function(res){
+              console.log(res.data);
+              getApp().globalData.header.Cookie = 'JSESSIONID=' + res.data;
+            }
           })
           // ------------------------------------
 
@@ -75,121 +82,5 @@ App({
       complete: function() {
       }
     })
-  },
-  //获取用户的登录状态
-  userIsLogin:function(){
-     wx.getStorage({
-      key: 'login',
-      success: function(res){
-       
-        if(res == null || res == {}){
-           that.globalData.islogin = false;
-        }else{
-          that.globalData.islogin = true;
-        }
-        console.log(that.globalData.islogin)
-      },
-      fail: function() {
-      },
-      complete: function() {
-      }
-    })
-  },
-  // 获取用户bid信息
-  getUserBid:function(res){
-    if(this.globalData.bid){
-      res(this.globalData.bid);
-      return;
-    }
-    var that = this;
-    wx.request({
-      url: 'http://www.jiuyunda.net:90/api/v1/city/current_city',
-      data: {
-        "province":that.globalData.city.province,
-        "city":that.globalData.city.city,
-        "district":that.globalData.city.district,
-      },
-      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      success: function(bid){
-        that.globalData.bid = bid.data;
-        res(bid.data);
-      },
-      fail: function() {
-      },
-      complete: function() {
-      }
-    })
-  },
-  // 获取位置信息
-  getUserLocation:function(res){
-   var that = this;
-   if(this.globalData.location){
-     res(this.globalData.location);
-     return;
-   }
-    wx.getLocation({
-      type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-      success: function(location){
-        that.globalData.location = location;
-        res(that.globalData.location);
-      },
-      fail: function() {
-        wx.showToast({
-          title:"获取位置失败",
-          icon:"success",
-          duration:350
-        })
-      },
-      complete: function() {
-      }
-    })
-  },
-  // 获取用户信息
-   getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
-  // 添加商品到购物车车
-  addGoodToShopCar:function(good){
-    var tempGood = this.globalData.shopCarGoods[good.id];
-    if(tempGood != null){
-        tempGood.buy = good.buy;
-        this.globalData.shopCarGoods[good.id] = tempGood;
-    }else{
-      this.globalData.shopCarGoods[good.id] = good
-    }
-  },
-  // 从购物车减少商品
-  reduceGoodFromShopCar:function(good){
-        console.log(this.globalData.shopCarGoods)
-    if(good.buy == 0){
-      delete(this.globalData.shopCarGoods[good.id])
-      console.log("删除")
-      console.log(this.globalData.shopCarGoods)
-      return;
-    }
-    var tempGood = this.globalData.shopCarGoods[good.id];
-    if(tempGood){
-      tempGood.buy = good.buy;
-      this.globalData.shopCarGoods[good.id] = tempGood;
-    }else{
-      this.globalData.shopCarGoods[good.id] = good
-    }
-    console.log(this.globalData.shopCarGoods[good.id])
-  }
+  }  
 })
---
+++ pages/newBuyer/newBuyer.js
@@ -0,0 +1,84 @@
+// pages/newBuyer/newBuyer.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  formSubmit: function (e) {
+    var that = this;
+    var formData = e.detail.value;
+    console.log(formData);
+    var header = getApp().globalData.header;
+    wx.request({
+      url: getApp().globalData.serverUrl+'buyerAction_insert.action',
+      data: formData,
+      header: {
+        'Content-Type': 'application/json',
+        'Cookie': header.Cookie 
+      },
+      success: function (res) {
+        console.log(res.data);
+        
+      }
+    })
+  }
+})
